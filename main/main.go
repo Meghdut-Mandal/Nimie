ac89fbc61eb83fec2cc1f4afa9f0541ae3129a7c
Meghdut Mandal <meghdut.mandal@autodesk.com>
2022-03-08 09:16:25 +0530

    redundant commit

--- main/main.go
+++ main/main.go
@@ -9,6 +9,8 @@ import (
 	"os"
 )
 
+// main function
+
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
